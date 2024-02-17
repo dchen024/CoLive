250ebc8b1679e342ccb446277a28602ee09a1d2b
Alex Chen <112523chen@gmail.com>
2024-02-17 10:58:06 -0500

    new(): update clerk and add supbase util

---
+++ src/app/home/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { SignOutButton } from "@clerk/nextjs";
+
+const page = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col items-center justify-between p-24">
+        <h1>Hello World!</h1>
+      </div>
+      <SignOutButton />
+    </div>
+  );
+};
+
+export default page;
