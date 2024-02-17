6609312b42d7482ecdd3d28dd255528439956725
Marouane <57333995+riimuru@users.noreply.github.com>
2024-02-16 22:24:33 -0500

    add typeform

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Typeform from "./pages/typeform";
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>Hello World!</h1>
+      <Typeform />
     </main>
   );
 }
